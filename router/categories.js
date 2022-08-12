36ddaea702bd0d46db16af97e767f7be75e8c5ff
Franco Baigorria <baigorriafrancoad@outlook.com>
2022-08-12 17:04:00 -0300

    Added: create new category

--- router/categories.js
+++ router/categories.js
@@ -18,4 +18,21 @@ categoriesRouter.get("/", async (req, res) => {
 	}
 });
 
+categoriesRouter.post("/", async (req, res) => {
+	const body = req.body;
+	const token = req.get("authorization");
+
+	try {
+		let user = jwt.verify(token, process.env.JWT_SECRET);
+		const [row, fields] = await pool.query(
+			"INSERT INTO `category` (`id`, `title`, `user_id`, `category_type_id`) VALUES (NULL, ?, ?, ?); ",
+			[body.title, user.user_id, body.category_type]
+		);
+
+		res.json({ status: "ok", id: row.insertId });
+	} catch (err) {
+		return res.json({ status: "error", error: ";))" });
+	}
+});
+
 module.exports = categoriesRouter;
